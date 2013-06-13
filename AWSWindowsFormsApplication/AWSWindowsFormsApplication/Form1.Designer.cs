d1319033b429070c4803a8a071065c47bd352e5e
IcodeonPOC <happiesticodeon@gmail.com>
2013-06-13 14:35:01 +0530

    Checking in POC for Amazon SQS
    Please find the windows app which writes and reads messages from/into
    Amazon SQS..
    This is created by Anantha,
    Pending for review.

---
+++ AWSWindowsFormsApplication/AWSWindowsFormsApplication/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AWSWindowsFormsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPushMsg = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click To PUSH Msg";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPushMsg
+            // 
+            this.txtPushMsg.Location = new System.Drawing.Point(83, 41);
+            this.txtPushMsg.Name = "txtPushMsg";
+            this.txtPushMsg.Size = new System.Drawing.Size(100, 20);
+            this.txtPushMsg.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Click to POP Msg";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtPushMsg);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPushMsg;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AWSWindowsFormsApplication/AWSWindowsFormsApplication/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Amazon.SQS;
+using Amazon;
+using Amazon.SQS.Model;
+
+namespace AWSWindowsFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        AmazonSQS sqs = AWSClientFactory.CreateAmazonSQSClient(RegionEndpoint.USWest2);
+        String myQueueUrl;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateQueueRequest sqsRequest = new CreateQueueRequest();
+            sqsRequest.QueueName = "MYFirstQueue";
+            CreateQueueResponse createQueueResponse = sqs.CreateQueue(sqsRequest);            
+            myQueueUrl = createQueueResponse.CreateQueueResult.QueueUrl;
+
+            //Confirming the queue exists
+            ListQueuesRequest listQueuesRequest = new ListQueuesRequest();
+            ListQueuesResponse listQueuesResponse = sqs.ListQueues(listQueuesRequest);
+            
+            SendMessageRequest sendMessageRequest = new SendMessageRequest();
+            sendMessageRequest.QueueUrl = myQueueUrl; 
+            sendMessageRequest.MessageBody = txtPushMsg.Text;
+            sqs.SendMessage(sendMessageRequest);
+        
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest();
+            receiveMessageRequest.QueueUrl = myQueueUrl;
+            ReceiveMessageResponse receiveMessageResponse = sqs.ReceiveMessage(receiveMessageRequest);
+            if (receiveMessageResponse.IsSetReceiveMessageResult())
+            {
+                ReceiveMessageResult receiveMessageResult = receiveMessageResponse.ReceiveMessageResult;
+                foreach (Amazon.SQS.Model.Message message in receiveMessageResult.Message)
+                {
+                    if (message.IsSetBody())
+                     MessageBox.Show(message.Body);
+                }
+            }
+
+            String messageRecieptHandle = receiveMessageResponse.ReceiveMessageResult.Message[0].ReceiptHandle;
+            DeleteMessageRequest deleteRequest = new DeleteMessageRequest();
+            deleteRequest.QueueUrl = myQueueUrl;
+            deleteRequest.ReceiptHandle = messageRecieptHandle;
+            sqs.DeleteMessage(deleteRequest);
+        }
+    }
+}
